58de2edf6b6b537fb9b5b504323b1994cf0b164f
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2026-05-18 20:58:40 +0530

    participate gogole

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ import withPWA from 'next-pwa';
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
+    removeConsole: true,
   },
 };
 
 export default withPWA({
   dest: 'public',
-  disable: true,
+  disable: false,
   register: true,
   skipWaiting: true,
 })(nextConfig);

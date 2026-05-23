4885b187b6a9039df1381cf03e8f86c2603eb16f
NIKHIL HAZARIKA <nikhilhazarika9@gmail.com>
2026-05-23 20:32:59 +0530

    fix assetlink

--- public/sw.js
+++ public/sw.js
@@ -1,100 +1,98 @@
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+define(['./workbox-e43f5367'], (function (workbox) {
+  'use strict';
 
-// If the loader is already loaded, just stop.
-if (!self.define) {
-  let registry = {};
-
-  // Used for `eval` and `importScripts` where we can't get script URL by other means.
-  // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri;
-
-  const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + ".js", parentUri).href;
-    return registry[uri] || (
-      
-        new Promise(resolve => {
-          if ("document" in self) {
-            const script = document.createElement("script");
-            script.src = uri;
-            script.onload = resolve;
-            document.head.appendChild(script);
-          } else {
-            nextDefineUri = uri;
-            importScripts(uri);
-            resolve();
-          }
-        })
-      
-      .then(() => {
-        let promise = registry[uri];
-        if (!promise) {
-          throw new Error(`Module ${uri} didn’t register its module`);
-        }
-        return promise;
-      })
-    );
-  };
-
-  self.define = (depsNames, factory) => {
-    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
-    if (registry[uri]) {
-      // Module is already loading or loaded.
-      return;
-    }
-    let exports = {};
-    const require = depUri => singleRequire(depUri, uri);
-    const specialDeps = {
-      module: { uri },
-      exports,
-      require
-    };
-    registry[uri] = Promise.all(depsNames.map(
-      depName => specialDeps[depName] || require(depName)
-    )).then(deps => {
-      factory(...deps);
-      return exports;
-    });
-  };
-}
-define(['./workbox-e43f5367'], (function (workbox) { 'use strict';
-
-  importScripts();
   self.skipWaiting();
   workbox.clientsClaim();
-  workbox.registerRoute("/", new workbox.NetworkFirst({
-    "cacheName": "start-url",
-    plugins: [{
-      cacheWillUpdate: async ({
-        request,
-        response,
-        event,
-        state
-      }) => {
-        if (response && response.type === 'opaqueredirect') {
-          return new Response(response.body, {
-            status: 200,
-            statusText: 'OK',
-            headers: response.headers
-          });
+
+  // Cache the start URL with NetworkFirst
+  workbox.registerRoute(
+    "/",
+    new workbox.NetworkFirst({
+      cacheName: "start-url",
+      plugins: [{
+        cacheWillUpdate: async ({ response }) => {
+          if (response && response.type === 'opaqueredirect') {
+            return new Response(response.body, {
+              status: 200,
+              statusText: 'OK',
+              headers: response.headers
+            });
+          }
+          return response;
         }
-        return response;
-      }
-    }]
-  }), 'GET');
-  workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
-    "cacheName": "dev",
-    plugins: []
-  }), 'GET');
+      }]
+    }),
+    'GET'
+  );
+
+  // Cache pages (HTML) - NetworkFirst so fresh content loads when online
+  workbox.registerRoute(
+    ({ request }) => request.mode === 'navigate',
+    new workbox.NetworkFirst({
+      cacheName: "pages",
+      networkTimeoutSeconds: 3,
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 20,
+          maxAgeSeconds: 24 * 60 * 60 // 1 day
+        })
+      ]
+    })
+  );
+
+  // Cache static assets (JS, CSS) - StaleWhileRevalidate
+  workbox.registerRoute(
+    ({ request }) =>
+      request.destination === 'script' ||
+      request.destination === 'style',
+    new workbox.StaleWhileRevalidate({
+      cacheName: "static-assets",
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 50,
+          maxAgeSeconds: 7 * 24 * 60 * 60 // 7 days
+        })
+      ]
+    })
+  );
+
+  // Cache images - CacheFirst (images rarely change)
+  workbox.registerRoute(
+    ({ request }) => request.destination === 'image',
+    new workbox.CacheFirst({
+      cacheName: "images",
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
+        })
+      ]
+    })
+  );
+
+  // Cache Google Fonts
+  workbox.registerRoute(
+    ({ url }) => url.origin === 'https://fonts.googleapis.com',
+    new workbox.StaleWhileRevalidate({ cacheName: "google-fonts-stylesheets" })
+  );
+
+  workbox.registerRoute(
+    ({ url }) => url.origin === 'https://fonts.gstatic.com',
+    new workbox.CacheFirst({
+      cacheName: "google-fonts-webfonts",
+      plugins: [
+        new workbox.ExpirationPlugin({
+          maxEntries: 30,
+          maxAgeSeconds: 365 * 24 * 60 * 60 // 1 year
+        })
+      ]
+    })
+  );
+
+  // API calls - NetworkOnly (always fresh, no caching)
+  workbox.registerRoute(
+    ({ url }) => url.pathname.startsWith('/api/'),
+    new workbox.NetworkOnly({ cacheName: "api" })
+  );
 
 }));
